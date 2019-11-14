3145195d0fbe1c4bfcdd3b02a2472f66803905cd
Hanchen Wang <57742636+hwang787@users.noreply.github.com>
2019-11-14 01:40:26 -0500

    Update test2.m

--- test2.m
+++ test2.m
@@ -1,27 +1,30 @@
 function [] = test2( )
-% This is a simple example to help you evaluate your clustering algo implementation. You should run your code several time and report the best
-% result. The data contains a 400*101 matrix call X, in which the last
-% column is the true label of the assignment, but you are not allowed to
-% use this label in your implementation, the label is provided to help you
-% evaluate your algorithm. 
-%
-%
-% Please implement your clustering algorithm in the other file, mycluster.m. Have fun coding!
-
 load('data');
 T = X(:,1:100);
 label = X(:,101);
-
+%display(T)
+%K=4
+%R=randperm(size(T,1),K);
+%%for i = 1:K
+    %clusters(i,:) = clusters(i,:)/sum(clusters(i,:))
+%end
 IDX = mycluster(T,4);
+display(IDX)
+
 
 acc=AccMeasure(label,IDX)
 
+
 % ======================== uncomment the following for extra task ========================
 % n_topics = None # TODO specify num topics yourself
-% load('nips')
+load('nips','wl')
+raw_count = X(:,1:100);
 
-% W = mycluster_extra(raw_count, n_topics)
-
+n_topics=10;
+[W] = mycluster_extra(raw_count, n_topics);
 % use show_topics to display your result
+show_topics(W,wl);
+%cc_extra=AccMeasure(wl,W);
+
 
 end
