f78d57117039ccbb43cbce1a951cd5a717af9902
Hanchen Wang <57742636+hwang787@users.noreply.github.com>
2019-11-14 01:10:11 -0500

    Add files via upload

---
+++ mycluster.m
@@ -0,0 +1,22 @@
+function [ class ] = mycluster( bow, K )
+%
+% Your goal of this assignment is implementing your own text clustering algo.
+%
+% Input:
+%     bow: data set. Bag of words representation of text document as
+%     described in the assignment.
+%
+%     K: the number of desired topics/clusters. 
+%
+% Output:
+%     class: the assignment of each topic. The
+%     assignment should be 1, 2, 3, etc. 
+%
+% For submission, you need to code your own implementation without using
+% any existing libraries
+
+% YOUR IMPLEMENTATION SHOULD START HERE!
+
+
+end
+
